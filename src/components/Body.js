fc792a47b6a72c671061df18a8774a530b5a154c
Jull <julljidw@gmail.com>
2021-10-03 16:55:56 -0600

    Componentes y aRREGLO

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import BookList from "./BookList"
+
+const Body = () => {
+    return (
+        <div>
+            <p>Cuerpo</p>
+            <BookList />
+        </div>
+    )
+}
+
+export default Body
